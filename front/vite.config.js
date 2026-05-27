4051f6b837309edfadcce5eafc1f06d1d9ad2c73
kuysaisus123 <your_github_pacharapa.2560@gmail.com>
2026-05-28 05:50:41 +0700

    force vite cache invalidate

--- front/vite.config.js
+++ front/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+    // build v2
+
 export default defineConfig({
   plugins: [
     react(),
